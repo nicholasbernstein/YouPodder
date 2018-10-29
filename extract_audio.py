a691dddd1bf01714223acd9a80ffbfeb2718d6ef
Nicholas Bernstein <np_be@nick-desktop.nicholasbernstein.local>
2018-10-29 14:48:43 -0700

    Added a new audio normalization option using ffmpeg directly. This was to handle bigger files. The pynormalize module gives more tuneable results, but runs out of memory with larger files. so, on something like a podcast, which is likely to be long a better way to do it is to run:
    extract_audio.py -u <url> -bn/--bignormalize

--- extract_audio.py
+++ extract_audio.py
@@ -18,8 +18,11 @@ parser.add_argument('-u', '--url', type=str, help="url to youtube video", requir
 parser.add_argument('-os', '--offset_start', type=int, help="start X seconds into audio, eg: --offset_start 60")
 #parser.add_argument('-t', '--truncate', type=int, help="truncate the file by X seconds, eg --truncate 60")
 parser.add_argument('-c', '--combine', action='store_true', help="Combine Bumpers to create a FINAL-filename.mp3")
-parser.add_argument('-n', '--normalize', action='store_true', help='Normalize audio volumes using ffmpeg libraries')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-n', '--normalize', action='store_true', help='Normalize audio volumes using ffmpeg libraries')
+group.add_argument('-bn', '--bignormalize', action='store_true', help='Normalize big file')
 parser.add_argument('-d', '--dbfs', type=float, help='optional setting for normalize, sets "target dialogue normalization fill scale", defaults to 0, -13.5 seems to work well too. I don\'t really know what this is doing, but you can look at the ffmpeg documentation, or just try a bunch of numbers and see what works.')
+
 
 args=parser.parse_args()
 
@@ -33,6 +36,11 @@ def combine_bumpers(audioname, outPutFile):
     
     outfile.close
     return True
+
+if args.dbfs is not None and args.normalize is None:
+    message = "you must use -n/--normalize when specifiying the dbfs, otherwise it doesn't make sense."
+    raise Exception(message)
+    str(message)
 
 cwd = os.getcwd()
 #url = sys.argv[1]
@@ -87,3 +95,9 @@ if args.normalize is True:
      #   Audio=outPutFile
     #   target_dbfs=target_dbfs,
     #)
+
+if args.bignormalize is True:
+    cmd =  'ffmpeg-normalize "' + outPutFile + '" -o "NORMALIZED\\' + outPutFile + '" -c:a mp3'
+    os.system(cmd)
+
+    
